5f7e47de7fd9710c185fe844af6b21ed708d48e9
Clever Junior <tk_clever.junior@outlook.com>
2022-07-08 22:58:41 -0400

    feat: adiciona segunda bateria de testes, e planetas ao mock
    - Os testes do requisito 2 fazem a cobertura do filtro de texto

---
+++ src/tests/02.TableTextFilter.spec.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+import { response as mockData } from './mocks/mockData';
+import userEvent from '@testing-library/user-event';
+
+const mockFetch = () => {
+  jest.spyOn(global, 'fetch')
+    .mockImplementation(() => Promise.resolve({
+      status: 200,
+      ok: true,
+      json: () => Promise.resolve(mockData),
+    }));
+}
+
+describe('2 - Crie um filtro de texto para a tabela', () => {
+  beforeEach(mockFetch);
+  afterEach(() => jest.clearAllMocks());
+
+  test('Testa se existe um campo de texto', () => {
+    render(<App />);
+
+    const inputText = screen.getByTestId('name-filter');
+
+    expect(inputText).toBeInTheDocument();
+  });
+
+  test('Testa se são filtrados os planetas que possuem a letra "o" no nome', () => {
+    render(<App />);
+    const inputText = screen.getByTestId('name-filter');
+
+    userEvent.type(inputText, 'o')
+
+    const planets = mockData.filter((planet) => planet.name.includes('o'));
+
+    planets.forEach((planet) => expect(screen.getByRole('cell', /`${planet}`/i)).toBeInTheDocument());
+
+    /* expect(screen.getByRole('cell', { name: /coruscant/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Dagobah/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Endor/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Hoth/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Kamino/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Naboo/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Tatooine/i })).toBeInTheDocument; */
+  });
+
+  test('Testa se são filtrados os planetas que possuem a letra "oo" no nome', () => {
+    render(<App />);
+    const inputText = screen.getByTestId('name-filter');
+
+    userEvent.type(inputText, 'oo')
+
+    expect(screen.getByRole('cell', { name: /Naboo/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Tatooine/i })).toBeInTheDocument;
+  });
+
+  test('Testa se ao digitar tatoo é filtrado o planeta Tatooine', () => {
+    render(<App />);
+    const inputText = screen.getByTestId('name-filter');
+
+    userEvent.type(inputText, 'taoo')
+
+    expect(screen.getByRole('cell', { name: /Tatooine/i })).toBeInTheDocument;
+  });
+
+  test('Realiza os dois filtros acima em sequência e após, testa a remoção do filtro por texto', () => {
+    render(<App />);
+    const inputText = screen.getByTestId('name-filter');
+
+    userEvent.type(inputText, 'oo')
+
+    expect(screen.getByRole('cell', { name: /Naboo/i })).toBeInTheDocument;
+    expect(screen.getByRole('cell', { name: /Tatooine/i })).toBeInTheDocument;
+
+    userEvent.type(inputText, '{backspace}');
+
+    const planets = mockData.filter((planet) => planet.name.includes('o'));
+
+    planets.forEach((planet) => expect(screen.getByRole('cell', /`${planet}`/i)).toBeInTheDocument());
+
+    userEvent.type(inputText, '{backspace}');
+
+    const table = screen.getByRole('table');
+
+    expect(table.lastChild.tagName).toBe('TBODY');
+    expect(table.lastChild.children.length).toBe(10);
+  });
+});
