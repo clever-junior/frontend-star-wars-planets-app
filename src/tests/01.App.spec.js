03b93265582e3730da3820f92e2cdee06edd5742
Clever Junior <tk_clever.junior@outlook.com>
2022-07-13 11:10:16 -0400

    feat: teste requisito 01
    - Adiciona função que renderiza a tabela após o fetch da api

--- src/tests/01.App.spec.js
+++ src/tests/01.App.spec.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
-import Table from '../components/Table';
+import { render, screen, act } from '@testing-library/react';
 import { response as mockData } from './mocks/mockData';
-import Provider from '../store/provider';
 import App from '../App';
 
 const mockFetch = () => {
@@ -13,19 +11,22 @@ const mockFetch = () => {
 }
 
 describe('1 - Faça uma requisição para API Star Wars no endpoint /planets', () => {
+  beforeEach(async () => {
+    mockFetch();
+
+    await act(async () =>{
+    render(<App />)
+    } )
+  });
+
   afterEach(() => jest.clearAllMocks());
-  beforeEach(mockFetch);
 
   test('Testa se uma requisição para a API é realizada', async () => {
-    render(<App />);
-
     expect(await global.fetch).toHaveBeenCalledTimes(1);
     expect(await global.fetch).toBeCalledWith('https://swapi-trybe.herokuapp.com/api/planets/');
   });
 
   test('Testa se é renderizada uma tabela e se ela tem 13 colunas ', async () => {
-    await render(<App />);
-
     const table = screen.getByRole('table');
 
     expect(table).toBeInTheDocument();
@@ -34,10 +35,7 @@ describe('1 - Faça uma requisição para API Star Wars no endpoint /planets', (
   });
 
   test('Testa se a tabela tem uma linha para cada planeta retornado', async () => {
-     await render(<App />);
-
      const table = screen.getByRole('table');
-
 
      expect(table.lastChild.tagName).toBe('TBODY');
      expect(screen.getAllByRole('row').length).toBe(11);
